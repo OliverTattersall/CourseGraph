71866996f71b8b64bd33447649090f34e7ca3f34
oliver tattersall <oliver.m.tattersall@gmail.com>
2024-05-23 13:41:44 -0400

    adding select option

--- webFrontend/course-grapher/src/app/courses.service.ts
+++ webFrontend/course-grapher/src/app/courses.service.ts
@@ -12,19 +12,19 @@ export class CoursesService {
       // this.courses.push( new Course(i.toString(), "Course " + i, Array.from({length: Math.floor(i*Math.random()/4)}, () => Math.floor(i*Math.random())) ) );
     }
     // this.courses.push( new Course("1", "Course 1", []));
-    // this.courses.push( new Course("2", "Course 2", [1]));
+    // this.courses.push( new Course("2", "Course 2", ["1"]));
     // this.courses.push( new Course("3", "Course 3", [1,2]));
 
     this.courses.push( new Course("PHYS121", "PHYS 121", []));
     this.courses.push( new Course("MATH138", "MATH 138", []));
     this.courses.push( new Course("PHYS122", "PHYS 122", ["PHYS121"]));
     this.courses.push( new Course("PHYS249", "PHYS 249", []));
-    this.courses.push( new Course("PHYS234", "PHYS 234", ["PHYS122", "PHYS249", "MATH138"]));
-    this.courses.push( new Course("AMATH271", "AMATH 271", ["PHYS121", "MATH138"]));
+    this.courses.push( new Course("PHYS234", "PHYS 234", ["PHYS122", "PHYS249", "MATH138"])); 
+    this.courses.push( new Course("AMATH271", "AMATH 271", ["MATH138", "PHYS121"]));
     this.courses.push( new Course("PHYS349", "PHYS 349", ["PHYS234", "PHYS249", "AMATH271"]));
    }
 
-  getCourses(): Course[] {
+  getCourses(endCourseId:string): Course[] {
     return this.courses;
   }
 
