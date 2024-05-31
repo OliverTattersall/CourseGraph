0df0b13294fbc71303fbd879be8f6749e18288ca
Owen Waldron <owen.r.waldron@gmail.com>
2024-05-31 17:18:21 -0400

    added partial req parsing for calendar

--- scrapingAndFormatting/course-scraper-calendar.js
+++ scrapingAndFormatting/course-scraper-calendar.js
@@ -2,6 +2,34 @@ const puppeteer = require('puppeteer');
 const baseUrl = 'https://uwaterloo.ca/academic-calendar/undergraduate-studies/catalog#/courses';
 
 
+// TODO: fix tree parsing
+const makeListIntoTree = async (page, prereqList) => {
+    let innerText = "" + await page.evaluate(prereqList => prereqList.innerText, prereqList);
+    console.log(innerText);
+    if (innerText == null) return null;
+
+    const matches = innerText.match('.+following')
+
+    if (!matches || matches.length == 0) {
+        return innerText;
+    }
+
+
+    const nextListItems = await prereqList.$$('li');
+    
+    const stepReqs = [];
+    for (let i = 0; i < nextListItems.length; i++) {
+        const nextListItem = nextListItems[i];
+        const req = await makeListIntoTree(page, nextListItem);
+        if (!!req) stepReqs.push(req);
+    }
+
+    const tree = {}
+    tree[matches[0]] = stepReqs;
+
+    return tree;
+}
+
 const scrapeCourse = async (courseCode) => {
     const len = courseCode.length;
     const faculty = courseCode.substring(0, len - 3).toUpperCase();
@@ -39,7 +67,11 @@ const scrapeCourse = async (courseCode) => {
     const description = await page.evaluate((descDiv => descDiv.innerText), descriptionDiv);
 
     // Find the prereqs div
-    const prereqsList = await page.waitForSelector('h3 ::-p-text(Description) + ul');
+    const prereqsListDiv = await page.waitForSelector('h3 ::-p-text(Prerequisites) + div');
+    const prereqList = await prereqsListDiv.$('li');
+    
+    // TODO: fix tree parsing
+    // let prereqTree = await makeListIntoTree(page, prereqList);
 
     
     course['code'] = courseCode;
@@ -48,6 +80,8 @@ const scrapeCourse = async (courseCode) => {
     course['classNumber'] = courseNumber;
     course['name'] = name;
     course['description'] = description;
+    // course['prereqTree'] = prereqTree;
+    // course['antireqTree'] = ;
     // course['prereqs'] = ;
     // course['antireqs'] = ;
     course['updatedAt'] = new Date();
