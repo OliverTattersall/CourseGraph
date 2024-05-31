ff92d45fdf8f054167ab33423348106858b0e884
Owen Waldron <owen.r.waldron@gmail.com>
2024-05-31 19:41:40 -0400

    added individual page parsing

--- scrapingAndFormatting/course-scraper-calendar.js
+++ scrapingAndFormatting/course-scraper-calendar.js
@@ -30,12 +30,46 @@ const makeListIntoTree = async (page, prereqList) => {
     return tree;
 }
 
+const scrapeCoursePage = async (page) => {
+    // Find the course name
+    const titleRef = await page.waitForSelector('h2 ::-p-text( - )');
+    const title = await page.evaluate(titleRef => titleRef.innerText, titleRef);
+    console.log(title);
+    const regexTitle = title.match('(.+) - (.+)');
+    const courseCode = regexTitle[1];
+    const name = regexTitle[2];
+
+    const regexCode = courseCode.match('([A-Z]+)(.+)');
+    const faculty = regexCode[1];
+    const courseNumber = regexCode[2];
+
+    // Find the description
+    const descriptionDiv = await page.waitForSelector('h3 ::-p-text(Description) + div');
+    const description = await page.evaluate((descDiv => descDiv.innerText), descriptionDiv);
+
+    // Find the prereqs div
+    const prereqsListDiv = await page.waitForSelector('h3 ::-p-text(Prerequisites) + div');
+    const prereqList = await prereqsListDiv.$('li');
+    
+    // TODO: fix tree parsing
+    // let prereqTree = await makeListIntoTree(page, prereqList);
+
+    const course = {};
+
+    course['code'] = courseCode;
+    course['faculty'] = faculty;
+    course['classNumber'] = courseNumber;
+    course['name'] = name;
+    course['description'] = description;
+
+    return course;
+}
+
 const scrapeCourse = async (courseCode) => {
     const len = courseCode.length;
     const faculty = courseCode.substring(0, len - 3).toUpperCase();
     const courseNumber = Number(courseCode.substring(len - 3));
     const humanCode = `${faculty} ${courseNumber}`;
-    const course = {};
 
     const browser = await puppeteer.launch({
         headless: false,
@@ -57,29 +91,8 @@ const scrapeCourse = async (courseCode) => {
     const courseLink = await page.waitForSelector(`a ::-p-text(${courseCode.toUpperCase()} - )`);
     await courseLink.click();
 
-    // Find the course name
-    const titleRef = await page.waitForSelector(`h2 ::-p-text(${courseCode.toUpperCase()} - )`);
-    const title = await page.evaluate(titleRef => titleRef.innerText, titleRef);
-    const name = title.substring(`${courseCode.toUpperCase()} - `.length);
-
-    // Find the description
-    const descriptionDiv = await page.waitForSelector('h3 ::-p-text(Description) + div');
-    const description = await page.evaluate((descDiv => descDiv.innerText), descriptionDiv);
-
-    // Find the prereqs div
-    const prereqsListDiv = await page.waitForSelector('h3 ::-p-text(Prerequisites) + div');
-    const prereqList = await prereqsListDiv.$('li');
+    const course = await scrapeCoursePage(page);
     
-    // TODO: fix tree parsing
-    // let prereqTree = await makeListIntoTree(page, prereqList);
-
-    
-    course['code'] = courseCode;
-    course['humanCode'] = humanCode;
-    course['faculty'] = faculty;
-    course['classNumber'] = courseNumber;
-    course['name'] = name;
-    course['description'] = description;
     // course['prereqTree'] = prereqTree;
     // course['antireqTree'] = ;
     // course['prereqs'] = ;
